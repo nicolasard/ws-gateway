71886d097236c70b5b18a0812d5bc81e6bcd6aea
Nicolas Ardison <nicolas.ard@gmail.com>
2023-01-21 13:21:48 +0100

    sssstarting to work

--- gateway-app/src/main/java/ar/servus/apiproxy/GatewayController.java
+++ gateway-app/src/main/java/ar/servus/apiproxy/GatewayController.java
@@ -1,22 +1,29 @@
 package ar.servus.apiproxy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class GatewayController {
 
+    private final Logger logger = LoggerFactory.getLogger(GatewayApp.class);
+
     @GetMapping("/**")
     String getController() {
-        return "Test";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject("http://192.168.193.47:8080/ns/WebServer?wsdl", String.class);
+        return result;
     }
 
     @PostMapping("/**")
     String postController(HttpEntity<String> httpEntity) {
         String json = httpEntity.getBody();
-        System.out.println(json);
+        logger.info(json);
         return "Test";
     }
 }
--- gateway-app/src/main/java/ar/servus/apiproxy/WebInterceptor.java
+++ gateway-app/src/main/java/ar/servus/apiproxy/WebInterceptor.java
@@ -1,16 +1,47 @@
 package ar.servus.apiproxy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.util.Enumeration;
 
+/*
+    Interceptor used for log the requests to our app at very first time and creates an UUID.
+ */
 public class WebInterceptor implements HandlerInterceptor {
+
+    private final Logger logger = LoggerFactory.getLogger(GatewayApp.class);
+
     @Override
     public boolean preHandle(
             HttpServletRequest request,
             HttpServletResponse response,
             Object handler) throws Exception {
+        StringBuffer jb = new StringBuffer();
+        logger.info("{} {}",request.getMethod(), request.getRequestURL());
+        Enumeration<String> headerNames = request.getHeaderNames();
+        logger.info("Query String: {}",request.getQueryString());
+        while (headerNames.hasMoreElements()) {
+            String param = headerNames.nextElement();
+            String paramValue = request.getHeader(param);
+            logger.info("Header: {}:{}",param,paramValue);
+        }
+        String line = null;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null)
+                jb.append(line);
+        } catch (Exception e) {
+            throw e;
+        }
+        logger.info(line);
+        logger.info("Response Status: {}",response.getStatus());
         return true;
     }
+
 }
